83cfefa0911ff9a20d061853818f35cae77c72c2
EduRibeiro00 <eribeiro306@gmail.com>
2020-02-17 11:29:42 +0000

    Start of lab1

---
+++ sdis_lab1/src/client/Client.java
@@ -0,0 +1,17 @@
+package client;
+import java.net.*;
+import java.util.HashMap;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+
+    public void sendRegisterRequest() {
+
+    }
+
+    public void sendLookupRequest() {
+
+    }
+}
---
+++ sdis_lab1/src/server/Server.java
@@ -0,0 +1,11 @@
+package server;
+import java.net.*;
+import java.util.HashMap;
+
+public class Server {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            P
+        }
+    }
+}
