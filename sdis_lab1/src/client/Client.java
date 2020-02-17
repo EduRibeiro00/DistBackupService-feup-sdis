22ea1b57884fd7d985f9d82141f92827e6e930e9
Miguel Pinto <pt.miguel99@gmail.com>
2020-02-17 19:18:10 +0000

    Added comments and class data members

--- sdis_lab1/src/client/Client.java
+++ sdis_lab1/src/client/Client.java
@@ -2,33 +2,58 @@ package client;
 import java.io.IOException;
 import java.net.*;
 
+/**
+ * Client class
+ */
 public class Client {
+    private static String host; // host address
+    private static int port; // port number
+    private static DatagramSocket socket; // socket that will be used for communication
+
+    /**
+     * Main Method
+     * @param args args[1] should specify host address, args[2] should specify host port
+     * @throws SocketException
+     * @throws UnknownHostException
+     */
     public static void main(String[] args) throws SocketException, UnknownHostException {
+        // check arguments
         if (args.length < 4) {
             System.out.println("Wrong number of arguments");
             System.exit(1);
         }
 
-        String host = args[1];
-        int port = Integer.parseInt(args[2]);
+        // set variables
+        host = args[1];
+        port = Integer.parseInt(args[2]);
         String oper = args[3];
         String dnsName = args[4];
 
-        DatagramSocket socket = new DatagramSocket(port);
+        // open socket
+        socket = new DatagramSocket(port);
 
+        // determine action
         if (oper == "register") {
             String ipAddress = args[5];
-            sendRegisterRequest(host, port, dnsName, ipAddress, socket);
+            sendRegisterRequest(dnsName, ipAddress, socket);
         } else if (oper == "lookup") {
-            sendLookupRequest(host, port, dnsName, socket);
+            sendLookupRequest(dnsName, socket);
         }
 
-        String response = receiveResponse(host, port, socket);
+        // receive and print response
+        String response = receiveResponse(socket);
         System.out.println(response);
         socket.close();
     }
 
-    private static void sendRegisterRequest(String host, int port, String dnsName, String ipAddress, DatagramSocket socket) throws UnknownHostException {
+    /**
+     * Method that sends a register request
+     * @param dnsName - DNS name to register
+     * @param ipAddress - IP address to which the DNS name will be registered
+     * @param socket - Communication socket
+     * @throws UnknownHostException
+     */
+    private static void sendRegisterRequest(String dnsName, String ipAddress, DatagramSocket socket) throws UnknownHostException {
         String request = "register " + dnsName + " " + ipAddress;
         byte[] buffer = request.getBytes();
         InetAddress address = InetAddress.getByName(host);
@@ -40,7 +65,13 @@ public class Client {
         }
     }
 
-    private static void sendLookupRequest(String host, int port, String dnsName, DatagramSocket socket) throws UnknownHostException {
+    /**
+     * Method that sends a lookup request
+     * @param dnsName - DNS name to register
+     * @param socket - Communication socket
+     * @throws UnknownHostException
+     */
+    private static void sendLookupRequest(String dnsName, DatagramSocket socket) throws UnknownHostException {
         String request = "lookup " + dnsName;
         byte[] buffer = request.getBytes();
         InetAddress address = InetAddress.getByName(host);
@@ -52,7 +83,13 @@ public class Client {
         }
     }
 
-    private static String receiveResponse(String host, int port, DatagramSocket socket) throws UnknownHostException {
+    /**
+     * Method that receives a response upon a request
+     * @param socket - Communication socket
+     * @return A string with the response
+     * @throws UnknownHostException
+     */
+    private static String receiveResponse(DatagramSocket socket) throws UnknownHostException {
         InetAddress address = InetAddress.getByName(host);
         byte[] buffer = new byte[1024];
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
