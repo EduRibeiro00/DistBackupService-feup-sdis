b65ed22eb23ff6fb81be6ca278c1aaf6f73d589b
luis ramos <deco.luis@live.com.pt>
2020-03-14 18:50:48 +0000

    added Message and Header classes and other useless ones

--- proj1/src/peer/Message.java
+++ proj1/src/peer/Message.java
@@ -1,4 +1,22 @@
 package peer;
 
+import java.util.Arrays;
+
+
 public class Message {
+    final String crlf = String.valueOf(0xD) + 0xA;
+    Header header;
+    byte[] body;
+
+    public Message(byte[] data) throws Exception {
+        String message = data.toString();
+        String[] split = message.split(this.crlf);
+
+        if (split.length != 2){
+            throw new Exception("Invalid message received");
+        }
+
+        this.header = new Header(Arrays.asList(split[0].split(" ")));
+        this.body = split[1].getBytes();
+    }
 }
