21d803331b34c9ea03a646af4394b860528895dd
luis ramos <deco.luis@live.com.pt>
2020-03-16 13:30:05 +0000

    added backup initiation method

--- proj1/src/peer/Peer.java
+++ proj1/src/peer/Peer.java
@@ -1,11 +1,8 @@
 package peer;
+
 import java.io.IOException;
 import java.net.*;
 import java.rmi.RemoteException;
-import java.security.NoSuchAlgorithmException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 import java.security.NoSuchAlgorithmException;
 
 
@@ -62,22 +59,6 @@ public class Peer implements RemoteInterface {
         }
 
         return replication;
-    }
-
-
-    /**
-     * Sends a backup message for peer-peer communication
-     * @param version the version of the protocol to be used
-     * @param senderId the ID of the message sender
-     * @param fileId the file identifier in the backup service, as the result of SHA256
-     * @param chunkNo the chunk number of the specified file (may be unsued)
-     * @param body the body of the message
-     * @throws IOException
-     * @throws NoSuchAlgorithmException
-     */
-    private void sendBackupMessage(String version, String senderId, String fileId, int chunkNo, int repDeg, String body) throws NoSuchAlgorithmException, IOException {
-        Message msg = new Message(version, MessageType.PUTCHUNK, senderId, fileId, chunkNo, repDeg, body);
-        msg.send(mcast_backup);
     }
 }
 
